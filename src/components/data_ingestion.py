2b5518cb9765d3c01cd48fbabf48338a0f886974
rishikeshgadge <rishikesh92003@gmail.com>
2025-07-04 23:58:24 +0530

    commit

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,7 +5,10 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from  dataclasses import dataclass
- 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+from src.components.modeltrainer import ModelTrainer,ModelTrainerConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifact','train.csv')
@@ -23,20 +26,27 @@ class DataIngestion:
             logging.info('read the dataset as the dataframe')
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
-            logging.info("triin test split initiated")
+            logging.info("train test split initiated")
+            ####
             train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
-            logging.info("INgestion of the data is completed")
+            logging.info("Ingestion of the data is completed")
+            ######
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
             )
         except Exception as e:
-            raise CustomException(e)
+            raise CustomException(e,sys)
             
 
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_path, test_path =   obj.initiate_data_ingestion()
+    
+    data_transformation = DataTransformation()
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_path, test_path)
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiateModelTrainer(train_arr,test_arr))
